8420fdd85ece3ede9193d57d12a3aca64e97857b
Cody Palmer <codyryanpalmer@gmail.com>
2022-11-13 10:10:10 -0600

    Updates to test project

--- CookieConsentProject/index.js
+++ CookieConsentProject/index.js
@@ -1,18 +1,14 @@
 const modal = document.getElementById("modal");
-
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500);
 
-/*
-Challenge:
-1. Take control of the close button.
-2. Use an event listener to set the display
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
+const modalOnClose = document.getElementById("modal-close-btn");
+modalOnClose.addEventListener('click', function() {
+    modal.style.display = 'none'
+});
 
-const exit = document.getElementById("modal-close-btn");
-onclick(
-    exit.style.display = 'none'
-)
+const sectionContainer = document.getElementById("section-container");
+sectionContainer.addEventListener('click', function() {
+    sectionContainer.style.display = 'none'
+});
