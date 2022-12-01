6321ca12552c3d6833046479cf5a0adc9e7bfaf3
Cody Palmer <codyryanpalmer@gmail.com>
2022-12-01 16:37:05 -0600

    Updates

--- CookieConsentProject/index.js
+++ CookieConsentProject/index.js
@@ -14,19 +14,8 @@ modalCloseBtn.addEventListener('click', function(){
 })
 
 declineBtn.addEventListener('mouseenter', function(){
-    modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
 })
-
-/*
-Challenge:
-1. Take control of the div holding the buttons.
-2. In index.css, set up a selector for a new class
-   that uses flex-direction to reverse the order
-   of its child elements.
-3. Toggle that class on the div holding the buttons
-   when a user's cursor hovers over the decline button.
-*/
-
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
