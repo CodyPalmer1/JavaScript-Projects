2ec6630e339adf580703735ff0ad96ced733847d
Cody Palmer <codyryanpalmer@gmail.com>
2022-12-04 21:09:30 -0600

    Practice with JS - import, for of, html through js

--- JSPractice/index.js
+++ JSPractice/index.js
@@ -1,3 +1,10 @@
+/*
+Challenge:
+1. Move the data to a file called data.js 
+   and import it into index.js.
+*/
+
+
 const catsData = [
     {
         emotionTags: ["moody"],
@@ -169,12 +176,30 @@ const catsData = [
     },
 ]
 
+const emotionRadios = document.getElementById('emotion-radios')
+
 function getEmotionsArray(cats){
     const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-            emotionsArray.push(cats[i].emotionTags[j])
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            emotionsArray.push(emotion)
         }
     }
-    console.log(emotionsArray)
+    return emotionsArray
 }
+
+
+function renderEmotionsRadios(cats){
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
+
+
+
+
