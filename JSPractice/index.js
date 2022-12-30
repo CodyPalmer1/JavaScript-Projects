c5150918f3adb7f62d4d44b4a7731cc51c7392f7
Cody Palmer <codyryanpalmer@gmail.com>
2022-12-29 20:40:46 -0600

    Updates to JSPractice project

--- JSPractice/index.js
+++ JSPractice/index.js
@@ -1,16 +1,32 @@
 import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
+
+getImageBtn.addEventListener('click', getMatchingCatsArray)
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
     for (let radio of radios){
         radio.classList.remove('highlight')
     }
-    // remove all instances of the highlight class
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function getMatchingCatsArray(){
+    /*
+    Challenge:
+    1. Add code to getMatchingCatsArray so
+       that the two existing lines of code
+       only run if an emotion has been selected.
+    */
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        console.log(selectedEmotion)
+    }
+
 }
 
 function getEmotionsArray(cats){
