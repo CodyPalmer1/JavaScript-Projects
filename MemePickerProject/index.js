94be6933ae82016b5963206e03ff1f22796045ee
Cody Palmer <codyryanpalmer@gmail.com>
2023-01-06 10:56:48 -0600

    Updates to Meme picker project

--- MemePickerProject/index.js
+++ MemePickerProject/index.js
@@ -1,3 +1,5 @@
+import { catsData } from '/data.js'
+
 const emotionRadios = document.getElementById('emotion-radios')
 
 function getEmotionsArray(cats){
@@ -10,25 +12,19 @@ function getEmotionsArray(cats){
     return emotionsArray
 }
 
-/*
-Challenge:
-1. Take control of the 'emotion-radios' div.
-2. In renderEmotionsRadios, set up a let
-   to hold our string of HTML. You can initialise
-   it with an empty string.
-3. Iterate over "emotions" and put each emotion
-   in a <p> tag and then add them to the let you
-   created in step 2.
-4. Render the string to the 'emotion-radios' div.
-*/
-
 function renderEmotionsRadios(cats){
+
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+
+        radioItems += `<input>${emotion}</p>`
     }
     emotionRadios.innerHTML = radioItems
 }
 
 renderEmotionsRadios(catsData)
+
+
+
+
