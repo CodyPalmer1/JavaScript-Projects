ae6b11326321f4974e725b1c841c479594914fdf
Cody Palmer <codyryanpalmer@gmail.com>
2023-01-17 01:13:28 -0600

    Working with e.target.id / classlist.remove & .add

--- MemePickerProject/index.js
+++ MemePickerProject/index.js
@@ -2,19 +2,17 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-emotionRadios.addEventListener('change', function(e){
-    console.log(e.target.id)
-})
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
 
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-<<<<<<< HEAD
             if (!emotionsArray.includes(emotion)){
-=======
-            if(!emotionsArray.includes(emotionsArray)){
->>>>>>> 4daa6347593a7d26d40bd6f15c8c6ac6a7ae17d2
                 emotionsArray.push(emotion)
             }
         }
