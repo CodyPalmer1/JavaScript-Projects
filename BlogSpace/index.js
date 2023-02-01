9f353933f804a1ea2ba0bc559dfe4e542070e353
Cody Palmer <codyryanpalmer@gmail.com>
2023-02-01 13:47:18 -0600

    Refactoring code in BlogSpace to reduce size

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -1,16 +1,22 @@
+let postsArray = []
+
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3 class="blah">${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
 
 document.getElementById("new-post").addEventListener("submit", function(e) {
@@ -34,7 +40,7 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
         .then(res => res.json())
         .then(post => {
             document.getElementById("blog-list").innerHTML = `
-                <h3 class="blah">${post.title}</h3>
+                <h3>${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
                 ${document.getElementById("blog-list").innerHTML}
