1f6d28033860ff534b0bd0cbe50859169872ec53
Cody Palmer <codyryanpalmer@gmail.com>
2023-02-01 13:49:44 -0600

    Additional Refactoring to BlogSpace

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -39,11 +39,7 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
         .then(post => {
-            document.getElementById("blog-list").innerHTML = `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-                ${document.getElementById("blog-list").innerHTML}
-            `
+            postsArray.unshift(post)
+            renderPosts()
         })
 })
