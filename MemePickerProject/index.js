1e241b338692ae9902905943c031fa5f340bbf42
Cody Palmer <codyryanpalmer@gmail.com>
2023-02-09 00:39:23 -0600

    Implemented users ability to click on emotion and return a meme as well as close out meme from modal button.

--- MemePickerProject/index.js
+++ MemePickerProject/index.js
@@ -1,12 +1,21 @@
-import {catsData} from './data.js'
+import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 getImageBtn.addEventListener('click', renderCat)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -21,33 +30,41 @@ function getMatchingCatsArray(){
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
         const isGif = gifsOnlyOption.checked
 
-        return catsData.filter(function (cat) {
+        const matchingCatsArray = catsData.filter(function(cat){
 
-            if (isGif) {
+            if(isGif){
                 return cat.emotionTags.includes(selectedEmotion) && cat.isGif
-            } else {
+            }
+            else{
                 return cat.emotionTags.includes(selectedEmotion)
             }
         })
+        return matchingCatsArray
     }
 }
 
 function getSingleCatObject(){
-
     const catsArray = getMatchingCatsArray()
 
-    if (catsArray.length === 1){
+    if(catsArray.length === 1){
         return catsArray[0]
     }
-    else {
+    else{
         const randomNumber = Math.floor(Math.random() * catsArray.length)
         return catsArray[randomNumber]
     }
-
 }
 
 function renderCat(){
-    getSingleCatObject() // temporary
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML =  `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
 }
 
 function getEmotionsArray(cats){
@@ -61,7 +78,6 @@ function getEmotionsArray(cats){
     }
     return emotionsArray
 }
-
 
 function renderEmotionsRadios(cats){
 
@@ -83,7 +99,3 @@ function renderEmotionsRadios(cats){
 }
 
 renderEmotionsRadios(catsData)
-
-
-
-
