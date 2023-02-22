987bb936fe3e0e05b584067c0f213ce3a7938ad2
Cody Palmer <codyryanpalmer@gmail.com>
2023-02-21 23:12:56 -0600

    made changes to fileprinter so that it reads the number of lines in the input file

--- JavaPracticeProject/FileProcessor.java
+++ JavaPracticeProject/FileProcessor.java
@@ -1,22 +1,23 @@
-package JavaPracticeProject;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FilePrinter {
+public class FileProcessor {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.err.println("Usage: java FilePrinter <filename>");
+            System.err.println("Usage: java FileProcessor <filename>");
             System.exit(1);
         }
 
         String filename = args[0];
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            int lineCount = 0;
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
+                lineCount++;
             }
+            System.out.println("Number of lines: " + lineCount);
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
             System.exit(1);
