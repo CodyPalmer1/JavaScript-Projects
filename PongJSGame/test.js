a04d11ea2df14d6dcb181e4a0fb8224181034d39
Cody Palmer <codyryanpalmer@gmail.com>
2023-03-10 15:07:38 -0600

    Functionality and playbility fixed with pongjs game

--- PongJSGame/test.js
+++ PongJSGame/test.js
@@ -1,19 +1,19 @@
-const gameBoard = document.querySelector('#gameBoard');
+const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector('#scoreText');
-const resetButton = document.querySelector('#resetButton');
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
-const boardBackground = '#forestgreen';
-const paddle1Color = '#FFA500';
-const paddle2Color = '#4CAB9C';
-const paddleBorder = '#000000';
-const ballColor = '#CCFF00';
-const BallBorderColor = '#000000';
-const ballRadius = 10;
+const boardBackground = "forestgreen";
+const paddle1Color = "lightblue";
+const paddle2Color = "red";
+const paddleBorder = "black";
+const ballColor = "yellow";
+const ballBorderColor = "black";
+const ballRadius = 12.5;
 const paddleSpeed = 50;
-let intervalId;
-let ballSpeed = 1;
+let intervalID;
+let ballSpeed;
 let ballX = gameWidth / 2;
 let ballY = gameHeight / 2;
 let ballXDirection = 0;
@@ -25,25 +25,25 @@ let paddle1 = {
     height: 100,
     x: 0,
     y: 0
-}
+};
 let paddle2 = {
     width: 25,
     height: 100,
     x: gameWidth - 25,
     y: gameHeight - 100
-}
+};
 
-window.addEventListener('keydown', changeDirection);
-resetButton.addEventListener('click', resetGame);
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
 
 gameStart();
 
 function gameStart(){
-    createBall()
-    nextTick()
-}
+    createBall();
+    nextTick();
+};
 function nextTick(){
-    intervalId = setTimeout(() => {
+    intervalID = setTimeout(() => {
         clearBoard();
         drawPaddles();
         moveBall();
@@ -51,11 +51,11 @@ function nextTick(){
         checkCollision();
         nextTick();
     }, 10)
-}
+};
 function clearBoard(){
     ctx.fillStyle = boardBackground;
     ctx.fillRect(0, 0, gameWidth, gameHeight);
-}
+};
 function drawPaddles(){
     ctx.strokeStyle = paddleBorder;
 
@@ -66,71 +66,72 @@ function drawPaddles(){
     ctx.fillStyle = paddle2Color;
     ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
     ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-}
+};
 function createBall(){
     ballSpeed = 1;
-    if(Math.round(Math.random()) === 1){
-        ballXDirection = 1;
-    } else {
+    if(Math.round(Math.random()) == 1){
+        ballXDirection =  1;
+    }
+    else{
         ballXDirection = -1;
     }
-    if(Math.round(Math.random()) === 1){
-        ballYDirection = 1;
-    } else {
-        ballYDirection = -1;
+    if(Math.round(Math.random()) == 1){
+        ballYDirection = Math.random() * 1; //more random directions
+    }
+    else{
+        ballYDirection = Math.random() * -1; //more random directions
     }
     ballX = gameWidth / 2;
     ballY = gameHeight / 2;
     drawBall(ballX, ballY);
-}
+};
 function moveBall(){
-    ballX += ballSpeed * ballXDirection;
-    ballY += ballSpeed * ballYDirection;
-}
+    ballX += (ballSpeed * ballXDirection);
+    ballY += (ballSpeed * ballYDirection);
+};
 function drawBall(ballX, ballY){
     ctx.fillStyle = ballColor;
-    ctx.strokeStyle = BallBorderColor;
+    ctx.strokeStyle = ballBorderColor;
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.fill();
-
-}
+};
 function checkCollision(){
-    if(ballY <= ballRadius){
+    if(ballY <= 0 + ballRadius){
         ballYDirection *= -1;
     }
     if(ballY >= gameHeight - ballRadius){
         ballYDirection *= -1;
     }
     if(ballX <= 0){
-        player2Score++;
+        player2Score+=1;
         updateScore();
         createBall();
         return;
     }
     if(ballX >= gameWidth){
-        player1Score++;
+        player1Score+=1;
         updateScore();
         createBall();
         return;
     }
-    if(ballX <= paddle1.x + paddle1.width + ballRadius){
+    if(ballX <= (paddle1.x + paddle1.width + ballRadius)){
         if(ballY > paddle1.y && ballY < paddle1.y + paddle1.height){
-            ballX = paddle1.x + paddle1.width + ballRadius; //If ball is stuck in paddle
+            ballX = (paddle1.x + paddle1.width) + ballRadius; // if ball gets stuck
             ballXDirection *= -1;
             ballSpeed += 1;
         }
     }
-    if(ballX >= paddle2.x - ballRadius){
+    if(ballX >= (paddle2.x - ballRadius)){
         if(ballY > paddle2.y && ballY < paddle2.y + paddle2.height){
-            ballX = paddle2.x - ballRadius; //If ball is stuck in paddle
+            ballX = paddle2.x - ballRadius; // if ball gets stuck
             ballXDirection *= -1;
             ballSpeed += 1;
         }
     }
-}
+};
 function changeDirection(event){
     const keyPressed = event.keyCode;
     const paddle1Up = 87;
@@ -139,31 +140,31 @@ function changeDirection(event){
     const paddle2Down = 40;
 
     switch(keyPressed){
-        case (paddle1Up):
+        case(paddle1Up):
             if(paddle1.y > 0){
                 paddle1.y -= paddleSpeed;
             }
             break;
-        case (paddle1Down):
+        case(paddle1Down):
             if(paddle1.y < gameHeight - paddle1.height){
                 paddle1.y += paddleSpeed;
             }
             break;
-        case (paddle2Up):
+        case(paddle2Up):
             if(paddle2.y > 0){
                 paddle2.y -= paddleSpeed;
             }
             break;
-        case (paddle2Down):
+        case(paddle2Down):
             if(paddle2.y < gameHeight - paddle2.height){
                 paddle2.y += paddleSpeed;
             }
             break;
     }
-}
+};
 function updateScore(){
     scoreText.textContent = `${player1Score} : ${player2Score}`;
-}
+};
 function resetGame(){
     player1Score = 0;
     player2Score = 0;
@@ -172,19 +173,19 @@ function resetGame(){
         height: 100,
         x: 0,
         y: 0
-    }
+    };
     paddle2 = {
         width: 25,
         height: 100,
         x: gameWidth - 25,
         y: gameHeight - 100
-    }
+    };
     ballSpeed = 1;
     ballX = 0;
     ballY = 0;
     ballXDirection = 0;
     ballYDirection = 0;
     updateScore();
-    clearInterval(intervalId);
+    clearInterval(intervalID);
     gameStart();
-}
+};
